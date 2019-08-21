8fb781a3f3dfa26b7a6982edf0dc34a860b4b31c
bh06176 <bh06176@CEIT2553214X014.ad.georgiasouthern.edu>
2019-08-21 16:43:10 -0400

    Added last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -6,7 +6,12 @@ public class FirstClassInGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Broderick!");
 		
+		String lastName;
 		
+		System.out.println("What is your last name?");
+		lastName = "Hammond";
+		
+		System.out.println(lastName);
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
