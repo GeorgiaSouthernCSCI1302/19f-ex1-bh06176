043517ffeb3e7695ffb95c700909978679816eff
bh06176 <bh06176@CEIT2553214X014.ad.georgiasouthern.edu>
2019-08-21 16:51:14 -0400

    Last name with scanner object

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,17 +1,22 @@
-
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, my name is Broderick!");
 		
+		Scanner kb = new Scanner(System.in);
 		String lastName;
 		
-		System.out.println("What is your last name?");
-		lastName = "Hammond";
+		System.out.println("\nWhat is your last name?");
+		lastName = kb.next();
 		
-		System.out.println(lastName);
+		
+		
+		//lastName = "\nMy last name is Hammond";
+		//System.out.println(lastName);
+		
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
