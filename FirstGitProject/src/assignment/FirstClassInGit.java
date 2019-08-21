974728ee821c3243f3fd70e3763ce3def4f9fc50
bh06176 <bh06176@CEIT2553214X014.ad.georgiasouthern.edu>
2019-08-21 16:34:47 -0400

    Display name in console

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,11 @@
+
 package assignment;
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, my name is Broderick!");
+		
 		
 		//add a statement to display your name in the console
 		
